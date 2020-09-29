874b275c452d5033dd64eacd6b0230647789251c
Mcl <mixool0204@gmail.com>
2020-09-29 22:08:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup nothing -s 0.0.0.0 -p ${PORT:=3000} -k ${PASSWORD:=password} -m ${METHOD:=rc4-md5} --plugin onthing --plugin-opts "server;path=${WSPATH:=/sspath}" >/dev/null 2>&1 &
+nothing -s 0.0.0.0 -p ${PORT:=3000} -k ${PASSWORD:=password} -m ${METHOD:=rc4-md5} --plugin onthing --plugin-opts "server;path=${WSPATH:=/sspath}" >/dev/null 2>&1
